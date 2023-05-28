a128776f7a29fdd4868e12d2e5e82e7054c5b04d
FcaioF <90013353+FcaioF@users.noreply.github.com>
2023-05-28 15:09:09 -0300

    Update s3_files.tf
    correção de caminho de pasta

--- terraform/s3_files.tf
+++ terraform/s3_files.tf
@@ -3,6 +3,6 @@ resource "aws_s3_bucket_object" "codigo_prova" {
     bucket = aws_s3_bucket.datalake_dev.id
     key = "prova-igti/spark/pratico_analise_enem2020.ipynb"
     acl = "private"
-    source = "terraform/arquivos_upload/pratico_analise_enem2020.ipynb"
-    etag = filemd5("terraform/arquivos_upload/pratico_analise_enem2020.ipynb")
+    source = "arquivos_upload/pratico_analise_enem2020.ipynb"
+    etag = filemd5("arquivos_upload/pratico_analise_enem2020.ipynb")
 }
