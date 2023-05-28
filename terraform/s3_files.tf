c56940f040a8853d29385978455e076cc1df3222
fcaiof <caiofranciscoferreira@outlook.com>
2023-05-28 14:45:58 -0300

    correção do caminho da pasta no repositorio usada para upload de codigos

--- terraform/s3_files.tf
+++ terraform/s3_files.tf
@@ -3,6 +3,6 @@ resource "aws_s3_bucket_object" "codigo_prova" {
     bucket = aws_s3_bucket.datalake_dev.id
     key = "prova-igti/spark/pratico_analise_enem2020.ipynb"
     acl = "private"
-    source = "/mnt/48447c52-52ac-4abb-be1b-d26a64732ee0/Documents/cursos/XP_Educação/engenheiro_de_dados_cloud/Modulo1/trabalho_pratico/pratico_analise_enem2020.ipynb"
-    etag = filemd5("/mnt/48447c52-52ac-4abb-be1b-d26a64732ee0/Documents/cursos/XP_Educação/engenheiro_de_dados_cloud/Modulo1/trabalho_pratico/pratico_analise_enem2020.ipynb")
+    source = "terraform/arquivos_upload/*"
+    etag = filemd5("terraform/arquivos_upload/*")
 }
