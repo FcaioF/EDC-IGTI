8fe7014ff6e99133189cc700d275e1e02f427fe0
fcaiof <caiofranciscoferreira@outlook.com>
2023-05-28 14:51:32 -0300

    inclusão do nome do arquivo que sera upado para s3

--- terraform/s3_files.tf
+++ terraform/s3_files.tf
@@ -3,6 +3,6 @@ resource "aws_s3_bucket_object" "codigo_prova" {
     bucket = aws_s3_bucket.datalake_dev.id
     key = "prova-igti/spark/pratico_analise_enem2020.ipynb"
     acl = "private"
-    source = "terraform/arquivos_upload/*"
-    etag = filemd5("terraform/arquivos_upload/*")
+    source = "terraform/arquivos_upload/pratico_analise_enem2020.ipynb"
+    etag = filemd5("terraform/arquivos_upload/pratico_analise_enem2020.ipynb")
 }
