1477da60504efb1bdd98c8a51751951cfcaa80e4
FcaioF <caiofranciscoferreira@outlook.com>
2023-06-17 19:53:43 -0300

    codigo usando para criar uma api usando a lib fastapi e deposi disponibilizar o acesso atraves de um servidor virtual unicorv

---
+++ Modulo 3/Docker/criando_Imagem_docker/src/api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+@app.get('/',response_class=HTMLResponse)
+async def root():
+    return """
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>Motos</title>
+</head>
+<body>
+  <h1>Sonhos de consumo</h1>
+  
+  <h2>Yamaha Tracer 900GT </h2>
+  <img src="https://www.zabikers.co.za/wp-content/uploads/2019/11/Tracer-Cover.jpg" alt="Imagem 1">
+  
+  <h2>Honda Africa Twin 1100</h2>
+  <img src="https://motos2022.pro.br/wp-content/uploads/2022/03/web-IMG_4515.png" alt="Imagem 2">
+  
+  <h2>Tiger 900 Rally</h2>
+  <img src="https://www.webmotors.com.br/wp-content/uploads/2023/03/03172715/Triumph-Tiger-900-Rally-Pro.webp" alt="Imagem 3">
+</body>
+</html>    
+    """
+
+@app.get('/healthz')
+async def health():
+    return {
+        "messagem":"application running correctly"
+            }
+
